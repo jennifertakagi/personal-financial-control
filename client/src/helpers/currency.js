48d0e12fb347af22feb727c49d7084815e5afb6d
jennifertakagi <jennitakagi@gmail.com>
2020-09-20 19:33:36 -0300

    feat: add function to format currency

---
+++ client/src/helpers/currency.js
@@ -0,0 +1,10 @@
+/**
+ * Formatted value as currency
+ * @param {number} value - Value to be formatted
+ * @param {string} currencyCountry - Country's currency type
+ * @param {string} currencyType - Country's type
+ * @returns {string} formatted value as currency, e.g: R$ 10,00
+ */
+export default function formattedCurrency(value = 0, currencyCountry = 'pt-BR', currencyType = 'BLR') {
+  return value.toLocaleString(currencyCountry, { style: 'currency', currency: currencyType });
+}
